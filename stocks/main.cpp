350577a980103f29eabd166a6a7f88c27485045f
Shikhar Vaish <shikhar.vaish90@gmail.com>
2022-05-07 19:50:47 -0400

    integrating stock data structures with calculator

--- stocks/main.cpp
+++ stocks/main.cpp
@@ -8,9 +8,17 @@
 #include "bootstrap.hpp"
 
 using namespace std;
+using namespace fre;
+
+double fRand(double fMin, double fMax)
+{
+	double f = (double)rand() / RAND_MAX;
+	return fMin + f * (fMax - fMin);
+}
 
 void execute_sampling_iteration(int thread_id, vector<vector<double>>& price, vector<double>& iwv, map<int, vector<vector<double>>>& thread_res) {
     Bootstrap trial;
+	cout << "D0" << endl;
 
     vector<vector<double>> res_ar = trial.CalculateAR(price, iwv);
 	cout << "D1" << endl;
@@ -28,6 +36,37 @@ void execute_sampling_iteration(int thread_id, vector<vector<double>>& price, ve
 	cout << "D4: " << thread_res.size() << endl;
 }
 
+StockDayData generate_random_stock_day_data() {
+	Date date;
+	double open = fRand(100, 200);
+	double high = fRand(100, 200);
+	double low = fRand(100, 200);
+	double close = fRand(100, 200);
+	double adj_close = fRand(100, 200);
+	long volume = rand() % 10 + 1;
+
+	StockDayData sd = StockDayData(date, open, high, low, close, adj_close, volume);
+	return sd;
+}
+
+Stock generate_random_stock(const int stockDayDataCount) {
+	string ticker = "AAPL";
+	string name = "Apple";
+	Date date;
+	Date period_ending;
+	double estimate = fRand(100, 200);
+	double reported = fRand(100, 200);
+	double surprise = fRand(100, 200);
+	double surprise_per = fRand(0, 1);
+
+	Stock s(ticker, name, date, period_ending, estimate, reported, surprise, surprise_per);
+	for (int i = 0; i < stockDayDataCount; i++) {
+		s.day_data_list.push_back(generate_random_stock_day_data());
+	}
+
+	return s;	
+}
+
 vector<double> generate_stock_data() {
 	vector<double> demo_stock;
 	demo_stock.push_back(127.3431);
@@ -41,10 +80,17 @@ vector<double> generate_stock_data() {
 
 int main()
 {
-    vector<vector<double>> price;
-
+	vector<Stock> stockList;
 	for (int i = 0; i < 80; i++) {
-		price.push_back(generate_stock_data());
+		stockList.push_back(generate_random_stock(5));
+	}
+
+    vector<vector<double>> price;
+	for (int i = 0; i < 80; i++) {
+		vector<double> priceList;
+		for (int j = 0; j < stockList[j].day_data_list.size(); j++)
+			priceList.push_back(stockList[i].day_data_list[j].adj_close);
+		price.push_back(priceList);
 	}
 
     vector<double> iwv;
--- stocks/stocks.cpp
+++ stocks/stocks.cpp
@@ -2,7 +2,6 @@
 
 using namespace fre;
 
-
 void Stock::reset(string ticker_, string name_, Date date_, Date period_ending_, double estimate_, double reported_, double surprise_, double surprise_percent_) {
         ticker = ticker_;
         name = name_;
@@ -14,3 +13,7 @@ void Stock::reset(string ticker_, string name_, Date date_, Date period_ending_,
         surprise_percent = surprise_percent_;
         day_data_list.clear();
       };
+
+void Stock::setDayData(vector<StockDayData> day_data_list_) {
+	day_data_list = day_data_list_;
+}
